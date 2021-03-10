c173739474cf6e5428c7360edb470473f4136bf4
Erik Larsson <lrssn95@gmail.com>
2021-03-10 22:32:36 +0100

    add and remove more files

--- LrssnEngine/src/LrssnEngine/Core/entrypoint.h
+++ LrssnEngine/src/LrssnEngine/Core/entrypoint.h
@@ -7,15 +7,15 @@ extern LrssnEngine::Application* LrssnEngine::createApplication();
 
 int main(int argc, char** argv){
     LrssnEngine::Log::init();
-    LE_PROFILE_BEGIN_SESSION("Startup", "HazelProfile-Startup.json");
+    LE_PROFILE_BEGIN_SESSION("Startup", "LrssnEngine-Profile-Startup.json");
     auto app = LrssnEngine::createApplication();
     LE_PROFILE_END_SESSION();
     
-    LE_PROFILE_BEGIN_SESSION("Runtime", "HazelProfile-Runtime.json");
+    LE_PROFILE_BEGIN_SESSION("Runtime", "LrssnEngine-Profile-Runtime.json");
     app->run();
     LE_PROFILE_END_SESSION();
 
-    LE_PROFILE_BEGIN_SESSION("Shutdown", "HazelProfile-Shutdown.json");
+    LE_PROFILE_BEGIN_SESSION("Shutdown", "LrssnEngine-Profile-Shutdown.json");
     delete app;
     LE_PROFILE_END_SESSION();
 }
