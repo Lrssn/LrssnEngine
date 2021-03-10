3d3fa1ef2ede8a3f0c2de90df1c9693b2b345150
Erik Larsson <lrssn95@gmail.com>
2021-03-10 22:25:43 +0100

    more files included due to new gitignore rules

---
+++ LrssnEngine/src/LrssnEngine/Debug/Instrumentor.h
@@ -0,0 +1,170 @@
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <thread>
+
+namespace LrssnEngine {
+	using FloatingPointMicroseconds = std::chrono::duration<double, std::micro>;
+	struct ProfileResult {
+		std::string Name;
+		FloatingPointMicroseconds Start;
+		std::chrono::microseconds ElapsedTime;
+		std::thread::id ThreadID;
+	};
+
+	struct InstrumentationSession {
+		std::string Name;
+	};
+
+	class Instrumentor 	{
+	private:
+		std::mutex mMutex;
+		InstrumentationSession* mCurrentSession;
+		std::ofstream mOutputStream;
+	public:
+		Instrumentor()
+			: mCurrentSession(nullptr) {
+		}
+
+		void BeginSession(const std::string& name, const std::string& filepath = "results.json") {
+			std::lock_guard lock(mMutex);
+			if (mCurrentSession) {
+				// If there is already a current session, then close it before beginning new one.
+				// Subsequent profiling output meant for the original session will end up in the
+				// newly opened session instead.  That's better than having badly formatted
+				// profiling output.
+				if (Log::GetCoreLogger()) { // Edge case: BeginSession() might be before Log::Init()
+					LE_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, mCurrentSession->Name);
+				}
+				InternalEndSession();
+			}
+			mOutputStream.open(filepath);
+			if (mOutputStream.is_open()) {
+				mCurrentSession = new InstrumentationSession({ name });
+				WriteHeader();
+			} else {
+				if (Log::GetCoreLogger()) { // Edge case: BeginSession() might be before Log::Init()
+					LE_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+				}
+			}
+		}
+
+		void EndSession() {
+			std::lock_guard lock(mMutex);
+			InternalEndSession();
+		}
+
+		void WriteProfile(const ProfileResult& result) {
+			std::stringstream json;
+
+			std::string name = result.Name;
+			std::replace(name.begin(), name.end(), '"', '\'');
+
+			json << ",{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
+			json << "\"name\":\"" << name << "\",";
+			json << "\"ph\":\"X\",";
+			json << "\"pid\":0,";
+			json << "\"tid\":" << result.ThreadID << ",";
+			json << "\"ts\":" << result.Start.count();
+			json << "}";
+
+			std::lock_guard lock(mMutex);
+			if (mCurrentSession) {
+				mOutputStream << json.str();
+				mOutputStream.flush();
+			}
+		}
+		static Instrumentor& Get() {
+			static Instrumentor instance;
+			return instance;
+		}
+	private:
+		void WriteHeader() {
+			mOutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
+			mOutputStream.flush();
+		}
+
+		void WriteFooter() {
+			mOutputStream << "]}";
+			mOutputStream.flush();
+		}
+		// Note: you must already own lock on m_Mutex before
+		// calling InternalEndSession()
+		void InternalEndSession() {
+			if (mCurrentSession) {
+				WriteFooter();
+				mOutputStream.close();
+				delete mCurrentSession;
+				mCurrentSession = nullptr;
+			}
+		}
+	};
+
+	class InstrumentationTimer {
+	public:
+		InstrumentationTimer(const char* name)
+			: mName(name), mStopped(false) {
+			mStartTimepoint = std::chrono::steady_clock::now();
+		}
+
+		~InstrumentationTimer() {
+			if (!mStopped)
+				Stop();
+		}
+
+		void Stop() {
+			auto endTimepoint = std::chrono::steady_clock::now();
+			auto highResStart = FloatingPointMicroseconds{ mStartTimepoint.time_since_epoch() };
+			auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(mStartTimepoint).time_since_epoch();
+
+			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
+			Instrumentor::Get().WriteProfile({ mName, highResStart, elapsedTime, std::this_thread::get_id() });
+
+			mStopped = true;
+		}
+	private:
+		const char* mName;
+		std::chrono::time_point<std::chrono::steady_clock> mStartTimepoint;
+		bool mStopped;
+	};
+}
+
+#define LE_PROFILE 1
+#if LE_PROFILE
+	// Resolve which function signature macro will be used. Note that this only
+	// is resolved when the (pre)compiler starts, so the syntax highlighting
+	// could mark the wrong one in your editor!
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define LE_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define LE_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define LE_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define LE_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define LE_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define LE_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define LE_FUNC_SIG __func__
+	#else
+		#define LE_FUNC_SIG "LE_FUNC_SIG unknown!"
+	#endif
+
+	#define LE_PROFILE_BEGIN_SESSION(name, filepath) ::LrssnEngine::Instrumentor::Get().BeginSession(name, filepath)
+	#define LE_PROFILE_END_SESSION() ::LrssnEngine::Instrumentor::Get().EndSession()
+	#define LE_PROFILE_SCOPE(name) ::LrssnEngine::InstrumentationTimer timer##__LINE__(name);
+	#define LE_PROFILE_FUNCTION() LE_PROFILE_SCOPE(LE_FUNC_SIG)
+#else
+	#define LE_PROFILE_BEGIN_SESSION(name, filepath)
+	#define LE_PROFILE_END_SESSION()
+	#define LE_PROFILE_SCOPE(name)
+	#define LE_PROFILE_FUNCTION()
+#endif 
